f99ca4f940e361e29bcd8af5222324f5e732c671
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-10-01 18:26:14 +0900

    chore

--- practice-fastify-1/src/app.ts
+++ practice-fastify-1/src/app.ts
@@ -1,17 +1,32 @@
-import Fastify from 'fastify'
+import Fastify, { FastifyInstance, FastifyPluginOptions } from 'fastify'
 import userRoutes from './modules/user/user.route'
+import helmet from '@fastify/helmet'
+import cors from '@fastify/cors'
+import fp from 'fastify-plugin'
+import { FastifyError } from 'fastify'
 
-const server = Fastify()
+const app = Fastify({
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        singleLine: true,
+      },
+    },
+  },
+})
 
-server.get('/healthcheck', async function (request, response) {
+app.get('/healthcheck', async function (request, response) {
   return { status: 'OK' }
 })
 
 async function main() {
-  server.register(userRoutes, { prefix: 'api/users' })
+  app.register(userRoutes, { prefix: 'api/users' })
+  app.register(helmet)
+  app.register(cors, { credentials: true })
 
   try {
-    await server.listen(3000, '0.0.0.0')
+    await app.listen(3000, '0.0.0.0')
     console.log('Server listening on port 3000')
   } catch (e) {
     console.error(e)
@@ -19,3 +34,13 @@ async function main() {
 }
 
 main()
+
+fp(function (
+  fastify: FastifyInstance,
+  _: FastifyPluginOptions,
+  done: (error?: FastifyError) => void
+): void {
+  fastify.decorateRequest('user', null)
+  fastify.addHook('onRequest', async (req, _) => {})
+  done()
+})
