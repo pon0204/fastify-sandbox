bce90599f95f2e46c0d97fb74f22f894ece77eee
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-10-02 10:40:19 +0900

    test

--- cloud-build-practice/app.js
+++ cloud-build-practice/app.js
@@ -3,6 +3,7 @@
 const path = require('path')
 const AutoLoad = require('@fastify/autoload')
 
+//
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
 
@@ -13,13 +14,13 @@ module.exports = async function (fastify, opts) {
   // through your application
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
+    options: Object.assign({}, opts),
   })
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
+    options: Object.assign({}, opts),
   })
 }
